c3427da43a7c5502fc072b25138da66befdf68c5
hugo566 <83823637+hugolgle@users.noreply.github.com>
2025-03-24 11:13:07 +0100

    serviceApiGet

---
+++ src/service/service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ServiceApi {
+  private url: string = 'https://opentdb.com/api.php';
+
+  constructor(private http: HttpClient) {}
+
+  public get(params: {
+    amount: number;
+    category: number;
+    difficulty: string;
+    type: string;
+    encode: string;
+  }) {
+    const httpParams = new HttpParams({
+      fromObject: {
+        amount: params.amount.toString(),
+        category: params.category,
+        difficulty: params.difficulty,
+        type: params.type,
+        encode: 'base64',
+      },
+    });
+
+    return this.http.get(this.url, { params: httpParams });
+  }
+}
