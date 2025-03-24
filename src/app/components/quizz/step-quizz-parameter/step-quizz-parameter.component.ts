654ab50c4f022fd3999e06b75fce3e53c5aed5b7
hugo566 <83823637+hugolgle@users.noreply.github.com>
2025-03-24 17:32:07 +0100

    stepQuizzParameter

--- src/app/components/quizz/step-quizz-parameter/step-quizz-parameter.component.ts
+++ src/app/components/quizz/step-quizz-parameter/step-quizz-parameter.component.ts
@@ -1,11 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonComponent } from '../../ui/button/button.component';
+import { TextInputComponent } from '../../ui/text-input/text-input.component';
+import { NumberInputComponent } from '../../ui/number-input/number-input.component';
+import { SelectComponent } from '../../ui/select/select.component';
 
 @Component({
   selector: 'app-step-quizz-parameter',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ButtonComponent,
+    NumberInputComponent,
+    SelectComponent,
+  ],
   templateUrl: './step-quizz-parameter.component.html',
-  styleUrl: './step-quizz-parameter.component.css'
+  styleUrls: ['./step-quizz-parameter.component.css'],
+  standalone: true,
 })
 export class StepQuizzParameterComponent {
+  amount: number = 0;
 
+  typeOptions: string[] = ['Type A', 'Type B', 'Type C'];
+  difficultyOptions: string[] = ['Facile', 'Moyen', 'Difficile'];
+
+  selectedType: string = '';
+  selectedDifficulty: string = '';
+
+  onSubmit(event: Event): void {
+    event.preventDefault();
+    console.log('Type sélectionné :', this.selectedType);
+    console.log('Difficulté sélectionnée :', this.selectedDifficulty);
+    console.log('Nombre de questions :', this.amount);
+  }
 }
---
+++ src/app/components/ui/number-input/number-input.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-number-input',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './number-input.component.html',
+  styleUrls: ['./number-input.component.css'],
+  standalone: true,
+})
+export class NumberInputComponent {
+  @Input() label: string = '';
+  @Input() value: number | null = null;
+  @Output() valueChange = new EventEmitter<number>();
+
+  onInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.valueChange.emit(Number(input.value));
+  }
+}
